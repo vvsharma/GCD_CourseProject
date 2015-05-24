d8e02b8a8973fcbdcc6384de919549c889528e5e
vvsharma <vvsharma007@gmail.com>
2015-05-24 00:32:19 -0500

    Initial commit

--- run_analysis.R
+++ run_analysis.R
@@ -11,10 +11,10 @@
 # folder itself has the reference data for the Activities and the Features being 
 # measured in the ".txt" files. The raw data files in the train and test folders
 # do not have any headers not do they have any linking columns (Ids and Foreign keys)
-# in the files. ASSUMPTION: If we go by the instructor's definition of the "Raw" data 
-# file, these files have not been manipulated in any way and the data collection 
-# has resulted in direct ono-to-one mapping between the rows in the files. This means 
-# we can reasonably use the row number as the linking element between the files. 
+# in the files. ASSUMPTION: If we go by the instructor's video from the first week, 
+# by definition, the "Raw" data have not been manipulated in any way. The data collection 
+# has resulted in direct ono-to-one mapping between the rows in the files each having the 
+# sme number of rows. 
 # That all the files have the same number of rows also points to this being a valid 
 # assumption.
 # 
@@ -23,8 +23,7 @@
 # for each of the variables. For eg. we will use the Activity-label files for the intermediate
 # activity dataset and the "Features.txt" to provide us the header for the measurement data. 
 # 
-# 
-# A single Tidy dataset is our first goal. \
+# A single combined Tidy dataset is our first goal.
 #
 # This data set is tidy because:
 #       1) All columns represent one and only one variable
@@ -32,17 +31,18 @@
 #       3) The final combined dataset is the only table for each 
 #          of the measurement variables.
 #       4) The combined dataset has descriptive column names.
-#       5) The Names of the variables should be all 
+#       5) The Names of the variables should be  
 #               a) lower cases 
-#               b) without underscores or dots or white spaces
+#               b) without punctuations like hyphens, underscores, dots or white spaces
 #               c) not duplicated
 #               d) be descriptive 
-#       (see Jeff's video on Week 4 - Editing Text Variables slide 16/16)                
+#       (There seems to be no restriction on the length of the variable's name)
+#       (see video of Week 4 - "Editing Text Variables" slide# 16)                
 #
 ##################################################################################
 
 ##################################################################################
-# We are in the "data" directory of the course project.
+# We are in the "UCI HAR Dataset" directory of the course project.
 # Within this directory, we have the test and train directories
 # which contain the subject, actvity, measurement data. 
 # We will also ignore the inertial_signals folder since it is not 
@@ -53,18 +53,18 @@
 # (in the features.txt) which can be used to provide "meaningful names" to
 # the measurements in x_test.txt and x_train.txt in their respective directories.
 ##################################################################################
-CourseProjectDataDir<-"~/Google Drive/Data Science/Getting and Cleaning Data/Course Project/UCI HAR Dataset"
-oldwd<-getwd()
+olddir<-getwd()
+MainDir<-"~/Google Drive/Data Science/Getting and Cleaning Data/Course Project/"
+DataDir<-paste(MainDir,"UCI HAR Dataset", sep="/")
+
 ################################################################
 # Create training dataset with Tidy Data principles.
 ################################################################
-setwd(CourseProjectDataDir)
-traindir<-"./train"
-setwd(traindir)
+TrainDir<-paste(DataDir,"train", sep="/")
+setwd(TrainDir)
 # Load the "train" subjects and apply a type of "train" to the new variable created called TestOrTrain
 trainsubjects<-read.table("subject_train.txt")  # Only the subjects chosen for training
 names(trainsubjects)<-"subjectid"
-
 #
 # Create Tidy data for activity dataset by loading the reference data and labeling the variables
 # We will use activityid to merge the datasets and we can drop it after.
@@ -93,24 +93,33 @@ features<-read.table("../features.txt")
 # step 1. Change  uppercase to lowercase letters.
 # We will do more cleaning to remove non-alphabet chracters after extracting 
 # only the mean() and std() data. 
-# It is my interpretation that the measures that do not have mean or std 
-# followed by the () can be discarded.
+# The measures that do not have mean or std followed by the () can be discarded.
 # 
 features2 <- tolower(features[,2])
-
 #
 names(rawdata)<-features2
+#
+# STEP 3 - Now drop the column variables that are not either "mean()", "meanfreq()" or "std()".
+# This also eliminates the last 7 angle variables which only use some mean items as parameters 
+# among the other variables that not mean or std.
+#
+# We will use the grep expression grep("((mean|std){1}(freq){0,}[(][)])",SAM)
+# to subset only the needed columns 
+rd<-rawdata[grep("((mean|std){1}(freq){0,}[(][)])",names(rawdata))]
+# VARIABLE NAME CLEANUP: 
+# As the final cleanup of the variable names replace the "-" with "_"
+# Apparently (from the 1st video from the 4th week) 
+# puntuation characters are not recommented in variable names.
+#
+names(rd)<-gsub("-|\\(\\)","",names(rd))
 #Now we have a tidy enough datasets to combine using cbind
-trainingsubjectactivityfeaturemeasures <- cbind(traineractivity,rawdata)
-
-
+trainingsubjectactivityfeaturemeasures <- cbind(traineractivity,rd)
 
 ################################################################
 # Setup "Test" subject data folder as current working directory
 ################################################################
-setwd(CourseProjectDataDir)     # reset to course project data dir
-testdir<-"./test"               # and move to test folder from there
-setwd(testdir)                  #
+TestDir<-paste(DataDir,"test", sep="/")  # and move to test folder from there
+setwd(TestDir)                  #
 
 # STEP 2 - Prepare the test subject data
 # in point 5 above on Tidy data. 
@@ -138,7 +147,6 @@ activity<-activity[][2]
 #Now we have the activity and "test" subjects, we can c-bind them
 testeractivity<-cbind(testsubjects,activity)
 #
-#
 rawdata<-read.table("x_test.txt", header = FALSE, sep = "", dec = ".")
 #add meaningful names from the features.txt as colnames for the test data
 features<-read.table("../features.txt")
@@ -146,32 +154,60 @@ features<-read.table("../features.txt")
 # step 1. Change  uppercase to lowercase letters.
 # We will do more cleaning to remove non-alphabet chracters after extracting 
 # only the mean() and std() data. 
-# It is my interpretation that the measures that do not have mean or std 
-# followed by the () can be discarded.
 # 
 features2 <- tolower(features[,2])
-
 #Apply the feature names as Column Names
 #
 names(rawdata)<-features2
-
-#Now we have a tidy enough datasets to combine using cbind
-testingsubjectactivityfeaturemeasures <- cbind(testeractivity,rawdata)
-
-
-setwd(CourseProjectDataDir) # reset to course project data dir
-#
-# RBind the training and training data
-#
-# SAM = SubjectActivityMeasures - is a data set that contains all the training and testing data.
-#
-SAM<- rbind(trainingsubjectactivityfeaturemeasures,testingsubjectactivityfeaturemeasures)
 #
 # STEP 3 - Now drop the column variables that are not either "mean()", "meanfreq()" or "std()".
-# This eliminates the last 7 angle variables which only use some mean items as parameters 
+# This also eliminates the last 7 angle variables which only use some mean items as parameters 
 # among the other variables that not mean or std.
 #
 # We will use the grep expression grep("((mean|std){1}(freq){0,}[(][)])",SAM)
 # to subset only the needed columns 
-sam<-SAM[grep("((mean|std){1}(freq){0,}[(][)])",names(SAM))]
+rd<-rawdata[grep("((mean|std){1}(freq){0,}[(][)])",names(rawdata))]
+# VARIABLE NAME CLEANUP: 
+# As the final cleanup of the variable names replace the "-" with "_"
+# Apparently (from the 1st video from the 4th week) 
+# puntuation characters are not recommented in variable names.
+#
+names(rd)<-gsub("-|\\(\\)","",names(rd))
+#Now we have a tidy enough datasets to combine using cbind
+testingsubjectactivityfeaturemeasures <- cbind(testeractivity,rd)
+#
+# STEP 4 - Produce the TIDY dataset by rbinding the training and training data
+#
+# SAM = SubjectActivityMeasures - is a data set that contains all the training and testing data.
+#
 
+SAM<- rbind(trainingsubjectactivityfeaturemeasures,testingsubjectactivityfeaturemeasures)
+
+
+# STEP 5 - deliverable
+
+#In this step we melt the feature measures into columns resulting in a tall table
+#
+require(reshape2)
+# reshape the dataset using melt and ddply into a tallntidy dataset  
+tallntidy<-melt(SAM, id=c("subjectid","activityname"))
+# and finally, group and summarize by mean function 
+require(plyr)
+y<-ddply(tallntidy, .(subjectid,activityname, variable), summarize, mean=mean(value))
+
+# write the file out as recommeded to the home/main folder (i.e. course project folder)
+# If the file is already there, overwrite it.
+#
+setwd(MainDir) # return to Main dir and create the Tidy file there.
+write.table(y, "TidyData.txt", row.name=FALSE)
+#Plesae note that the row.name=FALSE will remove any file headers.
+
+# NOTE TO PEER EVALUATORS:  
+#
+# The following code will read the TidyData.txt file back and re-apply the colnames.
+# Remove the comments and source the lines to use them
+#
+
+# td<- read.table("TidyData.txt")
+# names(td) <- c("subjectid", "activityname", "featuremeasured", "value")
+# str(td)  
